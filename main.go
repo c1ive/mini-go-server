199c9517470ec5ac68ad643bacf190036e2d4455
c1ive <sasowski@outlook.de>
2026-06-07 17:37:22 +0200

    Add workers

--- main.go
+++ main.go
@@ -6,10 +6,27 @@ import (
 	"strings"
 )
 
-func createHttpResponse(body string) []byte {
+type job struct {
+	connection net.Conn
+}
+
+func statusText(code int) string {
+	switch code {
+	case 200:
+		return "200 OK"
+	case 403:
+		return "403 Forbidden"
+	case 404:
+		return "404 Not Found"
+	default:
+		return "500 Internal Server Error"
+	}
+}
+
+func createHttpResponse(code int, body string) []byte {
 	return []byte(
-	"HTTP/1.1 200 OK\r\n" +
-	"Content-Type: text/plain\r\n" +
+	"HTTP/1.1 " + statusText(code) + "\r\n" +
+	"Content-Type: text/html\r\n" +
 	fmt.Sprintf("Content-Length: %d\r\n", len(body)) +
 	"\r\n" +
 	body,
@@ -31,18 +48,22 @@ func parseHttpRequest(httpRequest string) (string, error) {
 	return "", fmt.Errorf("Failed to parse HTTP")
 }
 
-func handlePath(path string) (string) {
+func handlePath(path string) (int, string) {
 	switch path {
 	case "/path":
-			return "You have made a GET request to /path!"
+			return 200, "<html><h1>PATH</h1><p>You have made a GET request to /path!</p></html>"
+	case "/admin":
+		return 403, "<html><h1>403 FORBIDDEN</h1><p>You are not allowed to be here...</p></html>"
+	case "/favicon.ico":
+		return 404, ""
 	case "/":
-		return "This is the GO server! Try out to navigate to /path"
+		return 200, "This is the GO server! Try out to navigate to /path"
 	default:
-		return "Error 404, resource not found :("
+		return 404, "Error 404, resource not found :("
 	}
 }
 
-func handleConnection(conn net.Conn, num int) {
+func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	fmt.Println("New connection from ", conn.RemoteAddr().String())
 
@@ -59,8 +80,14 @@ func handleConnection(conn net.Conn, num int) {
 		return
 	}
 
-	body := handlePath(path)
-	conn.Write(createHttpResponse(body))
+	responseCode, body := handlePath(path)
+	conn.Write(createHttpResponse(responseCode, body))
+}
+
+func worker(jobs chan job) {
+	for job := range jobs {
+		handleConnection(job.connection)
+	}
 }
 
 func main() {
@@ -71,16 +98,22 @@ func main() {
 		fmt.Println("Successfully created Listener!")
 	}
 
-	connections := 0
+	jobs := make(chan job, 10)
+
+	for i := 0; i < 3; i++ {
+		go worker(jobs)
+		fmt.Printf("Worker %d started\n", i)
+	}
 
 	for {
 		conn, err := ln.Accept()
-		connections++
+
 		if err != nil {
 			fmt.Println("Failed to accept connection!")
 		} else {
 			fmt.Println("Successfully accepted connection!")
 		}
-		go handleConnection(conn, connections)
+		
+		jobs <- job{conn}
 	}
 }
